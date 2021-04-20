2c4170af09ae312842890432c1b3723f2e6b5152
Vyacheslav Sokolov <vycheslavsokolov2013@gmail.com>
2021-04-20 14:23:46 +0600

    added tinkoff_currency_rates_v1

--- main.go
+++ main.go
@@ -1,68 +1,29 @@
 package main
 
-import (
-	"encoding/json"
-	"fmt"
-	"io/ioutil"
-	"log"
-	"net/http"
-)
-
-type SuccessResponseFromTinkoffCurrencyRates struct {
-	ResultCode string `json:"resultCode"`
-	Payload    struct {
-		LastUpdate struct {
-			Milliseconds int `json:"milliseconds"`
-		} `json:"lastUpdate"`
-		Rates []struct {
-			Category     string `json:"category"`
-			FromCurrency struct {
-				Code    int    `json:"code"`
-				Name    string `json:"name"`
-				StrCode string `json:"strCode"`
-			} `json:"fromCurrency"`
-			ToCurrency struct {
-				Code    int    `json:"code"`
-				Name    string `json:"name"`
-				StrCode string `json:"strCode"`
-			} `json:"toCurrency"`
-		} `json:"rates"`
-		Buy  float64 `json:"buy"`
-		Sell float64 `json:"sell"`
-	} `json:"payload"`
-	TrackingId string `json:"trackingId"`
-}
-
 func main() {
-	req, err := http.NewRequest("GET", "https://api.tinkoff.ru/v1/currency_rates", nil)
+	var params map[string]string
+
+	params = map[string]string{
+		//"from": "RUB",
+		//"to":   "USD",
+	}
+
+	response, err := getCurrencyRates(params)
 
 	if err != nil {
 		panic(err)
 	}
 
-	req.URL.Query().Add("from", "USD")
-	req.URL.Query().Add("to", "RUB")
+	rates := filterRates(response.Payload.Rates, func(rate RateFromResponse) bool {
+		fromCondition, _ := inArray(rate.FromCurrency.Name, []string{"RUB", "EUR", "USD"})
+		toCondition, _ := inArray(rate.ToCurrency.Name, []string{"RUB", "EUR", "USD"})
 
-	httpClient := http.Client{}
-	resp, err := httpClient.Do(req)
+		if fromCondition && toCondition && rate.Category == "DebitCardsOperations" {
+			return true
+		}
 
-	if err != nil || resp.StatusCode != 200 {
-		panic(err)
-	}
+		return false
+	})
 
-	body, err := ioutil.ReadAll(resp.Body)
-
-	if err != nil {
-		log.Fatalln(err)
-	}
-
-	parsedResponse := SuccessResponseFromTinkoffCurrencyRates{}
-
-	err = json.Unmarshal(body, &parsedResponse)
-
-	if err != nil {
-		panic(err)
-	}
-
-	fmt.Println(parsedResponse)
+	response.Payload.Rates = rates
 }
