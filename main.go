483463a01c769d7e59a04922141cccbdd224e3b6
Vyacheslav Sokolov <vycheslavsokolov2013@gmail.com>
2021-04-24 00:10:39 +0600

    added gocron

--- main.go
+++ main.go
@@ -1,7 +1,13 @@
 package main
 
+import (
+	"github.com/jasonlvhit/gocron"
+)
+
 func main() {
-	updateBankRates()
+	gocron.Every(1).Day().At("00:00:00").Do(updateBankRates)
+
+	<-gocron.Start()
 }
 
 func updateBankRates() {
