889ccd6e3d5764134bf9417032d24941a95772ff
Vyacheslav Sokolov <vycheslavsokolov2013@gmail.com>
2021-04-27 14:45:37 +0600

    added port as param

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 	_ "github.com/lib/pq"
 	log "github.com/sirupsen/logrus"
 	"net/http"
+	"os"
 )
 
 func main() {
@@ -24,9 +25,8 @@ func main() {
 		fmt.Fprintf(w, "Welcome to this life-changing API.\nIts the best API, its true, all other API's are fake.")
 	})
 
-	//appPort := os.Getenv("APP_PORT")
-
-	err = http.ListenAndServe(":8888", r)
+	appPort := os.Getenv("APP_PORT")
+	err = http.ListenAndServe(fmt.Sprintf(":%s", appPort), r)
 
 	if err != nil {
 		log.Error(err)
