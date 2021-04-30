1aeb3621d3f8f5a3d26a470097aefcb524906960
Vyacheslav Sokolov <vycheslavsokolov2013@gmail.com>
2021-05-01 00:59:05 +0600

    added time to run cron task at concrete time

--- cron.go
+++ cron.go
@@ -5,7 +5,8 @@ import (
 )
 
 func registerCronJobs() {
-	gocron.Every(1).Day().Do(updateBankRates)
+	gocron.Every(1).Day().At("00:00:00").Do(updateBankRates)
+	//gocron.Every(20).Seconds().Do(updateBankRates)
 
 	<-gocron.Start()
 }
