d62c9c7a1cfbd443b1a6c5087e1671b56d28c809
Vyacheslav Sokolov <vycheslavsokolov2013@gmail.com>
2021-04-30 14:07:49 +0600

    added param DB_HOST

--- database.go
+++ database.go
@@ -17,8 +17,9 @@ func getDatabaseSecrets() (string, string) {
 func getConnectionStr() string {
 	userName := os.Getenv("DB_NAME")
 	pass := os.Getenv("DB_PASSWORD")
-	dbName := os.Getenv("DB_PASSWORD")
+	dbName := os.Getenv("DB_NAME")
 	sslMode := os.Getenv("DB_SSL_MODE")
+	host := os.Getenv("DB_HOST")
 
-	return fmt.Sprintf("user=%s password=%s dbname=%s sslmode=%s", userName, pass, dbName, sslMode)
+	return fmt.Sprintf("user=%s password=%s dbname=%s sslmode=%s host=%s", userName, pass, dbName, sslMode, host)
 }
