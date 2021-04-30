09d9d29b09bcd81d145ef39033a4afc476de6a46
Vyacheslav Sokolov <vycheslavsokolov2013@gmail.com>
2021-04-30 22:09:20 +0600

    added annotation to json-serialize for model Rate

--- rate_model.go
+++ rate_model.go
@@ -1,14 +1,14 @@
 package main
 
 type Rate struct {
-	Id             int
-	BankId         int
-	Category       string
-	FromCurrencyId int
-	ToCurrencyId   int
-	Buy            int
-	Sell           int
-	CreatedAt      int //unix timestamp
+	Id             int    `json:"id"`
+	BankId         int    `json:"bank_id"`
+	Category       string `json:"category"`
+	FromCurrencyId int    `json:"from_currency_id"`
+	ToCurrencyId   int    `json:"to_currency_id"`
+	Buy            int    `json:"buy"`
+	Sell           int    `json:"sell"`
+	CreatedAt      int    `json:"created_at"`
 }
 
 func createBankRateModel(category string, fromCurrencyId, toCurrencyId, createdAt, bankId int, buy, sell float32) Rate {
