ba893a3b3dab10b3c22f1cf2d1cc5ed1a667dda8
Vyacheslav Sokolov <vycheslavsokolov2013@gmail.com>
2021-05-01 01:17:45 +0600

    changed response for hello-endpoint

--- server.go
+++ server.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/gorilla/mux"
 	log "github.com/sirupsen/logrus"
@@ -12,7 +13,10 @@ func registerServer() {
 	r := mux.NewRouter()
 
 	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Welcome to this life-changing API.\nIts the best API, its true, all other API's are fake.")
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		serializedResponse, _ := json.Marshal(map[string]string{"msg": "Welcome to this life-changing API.\nIts the best API, its true, all other API's are fake."})
+		fmt.Fprintf(w, string(serializedResponse))
 	})
 
 	appPort := os.Getenv("APP_PORT")
